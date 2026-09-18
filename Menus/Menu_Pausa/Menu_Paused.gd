9a73577afb2efcc1e2f730dfe45530672281f7e6
ishmaelprohay-code <ishmaelprohay@gmail.com>
2026-09-18 13:07:59 -0300

    asd

--- Menus/Menu_Pausa/Menu_Paused.gd
+++ Menus/Menu_Pausa/Menu_Paused.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 func _input(event):
 	if event.is_action_pressed("esc"):
 		$Pausad.visible = true
+		$BG.visible = true
 		get_tree().paused = true
 
 
@@ -13,9 +14,11 @@ func _input(event):
 
 func _on_Cont_pressed():
 	$Pausad.visible= false
+	$BG.visible = false
 	get_tree().paused = false
 
 
 func _on_Sal_pressed():
+	$BG.visible = false
 	get_tree().paused = false
 	get_tree().change_scene("res://Menus/Menu_principal/Menu_principal.tscn")
