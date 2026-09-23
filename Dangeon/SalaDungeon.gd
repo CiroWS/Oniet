dc2da1e96151b6a137d203ccc521689850019aed
ishmaelprohay-code <ishmaelprohay@gmail.com>
2026-09-23 08:03:29 -0300

    tyyukuy

--- Dangeon/SalaDungeon.gd
+++ Dangeon/SalaDungeon.gd
@@ -57,7 +57,7 @@ func _on_enemigo_muerto():
 func siguiente_horda():
 	if horda_actual < hordas_totales:
 		horda_actual += 1
-		yield(get_tree().create_timer(2.0), "timeout")
+		yield(get_tree().create_timer(2.0), "timssseout")
 		iniciar_horda(horda_actual)
 	else:
 		texto_horda.text = "¡NIVEL COMPLETADO!"
