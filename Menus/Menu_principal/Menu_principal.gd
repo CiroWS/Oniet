5e5d8be1cc3332d83dcc0539a6b523fb9fb4d1a0
CiroWS <cwendlersavino@gmail.com>
2026-09-24 11:17:10 -0300

    inicio

--- Menus/Menu_principal/Menu_principal.gd
+++ Menus/Menu_principal/Menu_principal.gd
@@ -8,7 +8,7 @@ func _on_Button2_pressed():
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Aula/Aula.tscn")
+	get_tree().change_scene("res://Menus/Menu_principal/anim/Animacion.tscn")
 
 
 func _on_musica_finished():
