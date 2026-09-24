02214eaf0a2e3ad2f6c0662b25294961f991797f
ishmaelprohay-code <ishmaelprohay@gmail.com>
2026-09-24 11:46:29 -0300

    hrehr

---
+++ Hud.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+
+
+
+
+func _ready():
+	$Control/HBoxContainer/Button.icon = preload("res://Armas/capacitor/capacitor bomba.png")
+	$Control/HBoxContainer/Button2.icon = preload("res://Armas/capacitor/capacitor bomba.png")
+	$Control/HBoxContainer/Button3.icon = preload("res://Armas/capacitor/capacitor bomba.png")
